887f9698fd905e4ac3dd3523a7bb7d6ea583612d
rhanzlick <rhanzlick90@gmail.com>
2024-02-12 10:21:30 -0500

    GitIngore and Solution Creation
    First Commit with files. - Recreation of the repo.

---
+++ LibraryRefactor/LibraryRefactor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryRefactor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
