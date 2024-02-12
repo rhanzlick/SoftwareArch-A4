c44c25d0d04d30cf46762d2b2d186ecfa35b8b6e
rhanzlick <rhanzlick90@gmail.com>
2024-02-12 10:40:06 -0500

    Modify Initial sln Setup
    Move Solution to git root path, update relative pathing to project files.

---
+++ LibraryRefactor/BlobLibrary/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace BlobLibrary
+{
+    public class Class1
+    {
+
+    }
+}
--- LibraryRefactor/LibraryRefactor/Program.cs
+++ LibraryRefactor/LibraryRefactor/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace LibraryRefactor
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
